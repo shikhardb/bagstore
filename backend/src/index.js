529806314b2891f2a850b84f73dc74470c06a313
Shikhar Dubey <shikhar.db@gmail.com>
2019-01-12 03:22:29 +0530

    Database created and initialized

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,20 @@
-// let's go!
+require('dotenv').config({ path: 'variable.env' });
+const createServer = require('createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO : use express middlewares to handle cookies (JWT)
+// TODO : use express middlewares to populate current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
+    },
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  }
+);
