143c4c75fd4d89a8ad4f547daec76e1b5585eaa0
Shikhar Dubey <shikhar.db@gmail.com>
2019-01-13 02:17:57 +0530

    Delete Items

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-
 import Title from './styles/Title';
 import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
+import DeleteItem from './DeleteItem';
 
 export default class Item extends Component {
   static propTypes = {
@@ -17,6 +17,7 @@ export default class Item extends Component {
     return (
       <ItemStyles>
         {item.image && <img src={item.image} alt={item.title} />}
+
         <Title>
           <Link
             href={{
@@ -29,6 +30,7 @@ export default class Item extends Component {
         </Title>
         <PriceTag>{formatMoney(item.price)}</PriceTag>
         <p>{item.description}</p>
+
         <div className="buttonList">
           <Link
             href={{
@@ -36,10 +38,10 @@ export default class Item extends Component {
               query: { id: item.id },
             }}
           >
-            <a>Edit ✏</a>
+            <a>Edit ✏️</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={item.id}>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
