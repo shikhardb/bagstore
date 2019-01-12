7942d501b8d8435f3f20bcfa1c8c8d69e91643c0
Shikhar Dubey <shikhar.db@gmail.com>
2019-01-12 22:50:49 +0530

    Items list

--- frontend/components/items.js
+++ frontend/components/items.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Item from './Item';
 
 const All_ITEMS_QUERY = gql`
   query All_ITEMS_QUERY {
@@ -41,7 +42,7 @@ class Items extends Component {
             return (
               <ItemsList>
                 {data.items.map(item => (
-                  <p>{item.title}</p>
+                  <Item item={item} key={item.id} />
                 ))}
               </ItemsList>
             );
